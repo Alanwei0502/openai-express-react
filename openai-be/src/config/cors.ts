e86e630ffabc7ca0ac522a68a2c41ec6d45f58f1
Alanwei0502 <alans40609@gmail.com>
2024-01-17 15:18:15 +0800

    fix cors

---
+++ openai-be/src/config/cors.ts
@@ -0,0 +1,24 @@
+import { CorsOptions, CorsOptionsDelegate, CorsRequest } from 'cors';
+
+const whitelist = [process.env.CLIENT];
+
+const corsOptionsDelegate: CorsOptionsDelegate = function (
+  req: CorsRequest,
+  callback: (error: Error | null, options: CorsOptions) => void,
+){
+  let corsOptions: CorsOptions;
+  let error = null;
+  const isDomainAllowed = whitelist.indexOf(req.headers['origin']!) !== -1;
+
+  if (isDomainAllowed) {
+    corsOptions = { origin: true };
+  }else{
+    corsOptions = { origin: false };
+    error = new Error('Not allowed by CORS');
+  }
+
+  corsOptions.methods = "GET, PUT, PATCH, POST, DELETE, OPTIONS";
+  callback(error, corsOptions);
+};
+
+export default corsOptionsDelegate;
--- openai-be/src/index.ts
+++ openai-be/src/index.ts
@@ -3,22 +3,22 @@ import express from 'express';
 import cors from 'cors';
 import apiRouter from './routes';
 import errorMiddleware from './middlewares/error.middleware';
+import corsOptionsDelegate from './config/cors';
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT,
-  methods: "GET, PUT, PATCH, POST, DELETE, OPTIONS",
-}));
+app.use(cors(corsOptionsDelegate));
 
+app.get('/healthz', async (_, res) => {
+  return res.send(new Date().toISOString() + ' health check');
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/');
 app.use('/api', apiRouter);
 app.use(errorMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '8081';
 app.listen(process.env.PORT, () => {
   console.log(`The server is running on port ${port}`);
 });
